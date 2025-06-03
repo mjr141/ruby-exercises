92408609c069ab0dc71057bfe32a21974c8b1c3d
mjr141 <204854489+mjr141@users.noreply.github.com>
2025-06-03 15:31:45 +0100

    complete 9_debugging

--- ruby_basics/9_debugging/spec/debugging_exercises_spec.rb
+++ ruby_basics/9_debugging/spec/debugging_exercises_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Debugging Exercises' do
   describe 'decrement_smallest_value' do
 
     # remove the 'x' from the line below to unskip the test
-    it 'Returns 0 after you use the stack trace to find and fix the error' do
+    xit 'Returns 0 after you use the stack trace to find and fix the error' do
       nested_array = [[3, 4], [1, 2]]
 
       smallest_value_decremented = decrement_smallest_value(nested_array)
@@ -38,7 +38,7 @@ RSpec.describe 'Debugging Exercises' do
   describe 'yell_greeting' do
 
     # remove the 'x' from the line below to unskip the test
-    xit 'Returns "WASSAP, BOB!" after you use pry-byebug to fix the method' do
+    it 'Returns "WASSAP, BOB!" after you use pry-byebug to fix the method' do
 
       expect(yell_greeting("Bob")).to eq("WASSAP, BOB!")
     end
